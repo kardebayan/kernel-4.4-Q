8509c76afb460486c18185b460ac17a4286befc5
Stanley Chu <stanley.chu@mediatek.com>
2019-02-28 07:16:05 +0800

    [ALPS04237610] ext4/hie: Fix missing HIE option
    The previous
    commit 052289846f6b ("[ALPS03949531] Merge branch android-4.4 into
    alps-trunk-p0.basic")
    removed HIE option incorrectly in ext4_get_encryption_info().
    
    Just add it back to fix booting failed issue.
    
    MTK-Commit-Id: 7497047e578067e1aeb037edb1da725a0706ed86
    
    Change-Id: I3c8b07f1136e43670d3abfe8bebaad7f8637e72d
    Signed-off-by: Stanley Chu <stanley.chu@mediatek.com>
    CR-Id: ALPS04237610
    Feature: [Android Default] EXT4 File System
    (cherry-pick from d46e51f311880f807672e610baba66c043bec8c5)

--- fs/ext4/crypto_key.c
+++ fs/ext4/crypto_key.c
@@ -288,6 +288,9 @@ int ext4_get_encryption_info(struct inode *inode)
 	case EXT4_ENCRYPTION_MODE_AES_256_HEH:
 		cipher_str = "heh(aes)";
 		break;
+	case EXT4_ENCRYPTION_MODE_PRIVATE:
+		cipher_str = "bugon";
+		break;
 	default:
 		printk_once(KERN_WARNING
 			    "ext4: unsupported key mode %d (ino %u)\n",
