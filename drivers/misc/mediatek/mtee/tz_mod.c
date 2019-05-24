37d95572bd189357eed94b595f9c72b2b53edc98
Seiya Wang <seiya.wang@mediatek.com>
2019-05-24 15:40:52 +0800

    [ALPS04401972] mtee: fix build error
    The prototype of get_user_pages has been changed. Fix it.
    
    MTK-Commit-Id: c142fa3d25012d38f9cac882d1384f045e4c4acb
    
    Change-Id: Ia01e0071c0796497c275fe52ebf493cc496b520c
    Feature: [Module]Kernel Maintenance
    CR-Id: ALPS04401972
    Signed-off-by: Seiya Wang <seiya.wang@mediatek.com>

--- drivers/misc/mediatek/mtee/tz_mod.c
+++ drivers/misc/mediatek/mtee/tz_mod.c
@@ -281,7 +281,7 @@ static long _map_user_pages(struct MTIOMMU_PIN_RANGE_T *pinRange,
 		return -ENOMEM;
 
 	pinRange->pageArray = (void *) pages;
-	write = (control == 0) ? 1 : 0;
+	write = (control == 0) ? FOLL_WRITE : 0;
 
 	/* Try to fault in all of the necessary pages */
 	down_read(&current->mm->mmap_sem);
@@ -294,13 +294,10 @@ static long _map_user_pages(struct MTIOMMU_PIN_RANGE_T *pinRange,
 		pinRange->isPage = 1;
 #if defined(CONFIG_MTEE_CMA_SECURE_MEMORY)
 		res = get_user_pages_durable(current, current->mm,
-					uaddr, nr_pages,
-					write, 0,/* don't force */
-					pages, NULL);
+					uaddr, nr_pages, write, pages, NULL);
 #else
 		res = get_user_pages(current, current->mm, uaddr, nr_pages,
-					write, 0,/* don't force */
-					pages, NULL);
+					write, pages, NULL);
 #endif
 	} else {
 		/* pfn mapped memory, don't touch page struct.
