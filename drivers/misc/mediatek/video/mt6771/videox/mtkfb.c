199404a3d18046950851a46e90a83b8f2cb0d5b5
tai-hua.tseng <tai-hua.tseng@mediatek.com>
2019-06-09 13:29:30 +0800

    [ALPS04454332] DISP: block display DVT ioctl
    [Detail]
    Block display ioctl MTKFB_CAPTURE_FRAMEBUFFER & MTKFB_SLT_AUTO_CAPTURE
    due to potiential security risk.
    Only enable these display ioctl on DVT stage.
    
    MTK-Commit-Id: da79613c63abd153e8ab1dfae4a93e0350504d26
    
    Change-Id: I5a719b11de12d09ba2f991938780de52e86df3be
    CR-Id:ALPS04454332
    Feature:  Display Driver (DSI Interface)
    Signed-off-by: tai-hua.tseng <tai-hua.tseng@mediatek.com>
    (cherry picked from commit e89507c16bb5d641809019d99bb5502c07d0ae4a)

--- drivers/misc/mediatek/video/mt6771/videox/mtkfb.c
+++ drivers/misc/mediatek/video/mt6771/videox/mtkfb.c
@@ -1336,7 +1336,7 @@ static int mtkfb_ioctl(struct fb_info *info, unsigned int cmd, unsigned long arg
 			up(&sem_early_suspend);
 			return r;
 		}
-
+#if defined(MTK_CAPTURE_SUPPORT)
 	case MTKFB_CAPTURE_FRAMEBUFFER:
 		{
 			unsigned long dst_pbuf = 0;
@@ -1425,6 +1425,7 @@ static int mtkfb_ioctl(struct fb_info *info, unsigned int cmd, unsigned long arg
 
 			return r;
 		}
+#endif
 
 	case MTKFB_GET_OVERLAY_LAYER_INFO:
 		{
@@ -1814,6 +1815,8 @@ static int mtkfb_compat_ioctl(struct fb_info *info, unsigned int cmd, unsigned l
 			DISPDBG("MTKFB_GET_POWERSTATE success %d\n", power_state);
 			break;
 		}
+
+#if defined(MTK_CAPTURE_SUPPORT)
 	case COMPAT_MTKFB_CAPTURE_FRAMEBUFFER:
 		{
 			compat_ulong_t __user *data32;
@@ -1827,6 +1830,8 @@ static int mtkfb_compat_ioctl(struct fb_info *info, unsigned int cmd, unsigned l
 			ret = mtkfb_ioctl(info, MTKFB_CAPTURE_FRAMEBUFFER, (unsigned long)pbuf);
 			break;
 		}
+#endif
+
 	case COMPAT_MTKFB_TRIG_OVERLAY_OUT:
 		{
 			arg = (unsigned long)compat_ptr(arg);
