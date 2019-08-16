3d4e30a397f7cab9ff578147aacbeddbee50299b
Ian-Y Chen <ian-y.chen@mediatek.com>
2019-08-16 22:31:22 +0800

    [ALPS04328846] power: fix boot_freq
    [Detail]
    mt6776 kernel 4.4 boot_freq cannot work
    
    [Solution]
    Remove redundant CONFIG_MTK_CPU_FREQ
    
    MTK-Commit-Id: cb4d5435c219b21b38091332da2d201c8bb6fa74
    
    Change-Id: Icb7875fb6adea97bc9cacb22061fca42e41c53b9
    CR-Id: ALPS04328846
    Feature: [Module]PowerHAL
    Signed-off-by: Ian-Y Chen <ian-y.chen@mediatek.com>

--- drivers/misc/mediatek/performance/boost_ctrl/cpu_ctrl/cpu_ctrl.c
+++ drivers/misc/mediatek/performance/boost_ctrl/cpu_ctrl/cpu_ctrl.c
@@ -278,7 +278,6 @@ static ssize_t perfmgr_boot_freq_proc_write(struct file *filp,
 			pr_debug("@%s: Invalid input: %s\n", __func__, tok);
 			goto out;
 		} else {
-#ifdef CONFIG_MTK_CPU_FREQ
 			if (i % 2) /* max */
 				freq_limit[i/2].max =
 					(data < 0 || data > MAX_NR_FREQ - 1)
@@ -290,7 +289,6 @@ static ssize_t perfmgr_boot_freq_proc_write(struct file *filp,
 					? -1 :
 					mt_cpufreq_get_freq_by_idx(i / 2, data);
 			i++;
-#endif
 		}
 	}
 
