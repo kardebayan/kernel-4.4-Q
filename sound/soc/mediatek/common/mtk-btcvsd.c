98fb0bc25ecdefa91250465ca0fc5f1c9d56255a
tina.tsai <tina.tsai@mediatek.com>
2019-09-27 11:55:45 +0800

    [ALPS04780087] btcvsd: add log in ISR for SPM
    [Detail]
    Add log in btcvsd IRQ handler for SPM debug:
    R12_CONN2AP_SPM_WAKEUP_B wake up host.
    Using log ratelimit, print once per 2s.
    
    MTK-Commit-Id: 5b5331b060cff6849b7e0fda425f7b21fa334b41
    
    Change-Id: Idfc92397eed53f2355787a60a3ae16ec124c8f8b
    Signed-off-by: Tina Tsai <tina.tsai@mediatek.com>
    CR-Id: ALPS04780087
    Feature: Wi-Fi Low Power

--- sound/soc/mediatek/common/mtk-btcvsd.c
+++ sound/soc/mediatek/common/mtk-btcvsd.c
@@ -503,6 +503,10 @@ static irqreturn_t mtk_btcvsd_snd_irq_handler(int irq_id, void *dev)
 	struct mtk_btcvsd_snd *bt = dev;
 	unsigned int packet_type, packet_num, packet_length;
 	unsigned int buf_cnt_tx, buf_cnt_rx, control;
+	static DEFINE_RATELIMIT_STATE(_rs, 2 * HZ, 1);
+
+	if (__ratelimit(&_rs))
+		dev_info(bt->dev, "%s(), irq_id=%d\n", __func__, irq_id);
 
 	if (bt->rx->state != BT_SCO_STATE_RUNNING &&
 	    bt->rx->state != BT_SCO_STATE_ENDING &&
