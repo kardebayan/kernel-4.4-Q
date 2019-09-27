6e6aaa92e907aa3245151cd99d8c10f1da672fc2
Carl Fang <carl.fang@mediatek.com>
2019-09-27 11:54:57 +0800

    [ALPS04676302] ccu: 6771 sensor decouple
    modify ccu for sensor dev decouple
    
    MTK-Commit-Id: 7016e31258328def7e51223a14c492364f9a9d91
    
    Change-Id: Id7c1f6d5fb34f338b1f74d3d3938509ff03abdea
    CR-Id: ALPS04676302
    Feature: [Android Default] AE (Auto Exposure)
    Signed-off-by: Carl Fang <carl.fang@mediatek.com>

--- drivers/misc/mediatek/ccu/src/mt6771/ccu_imgsensor.c
+++ drivers/misc/mediatek/ccu/src/mt6771/ccu_imgsensor.c
@@ -20,8 +20,10 @@
 /*>>>>> Information get from imgsensor driver*/
 static int32_t g_ccu_sensor_current_fps[IMGSENSOR_SENSOR_IDX_MAX_NUM] = {-1};
 #define SENSOR_NAME_MAX_LEN 50
-static struct ccu_sensor_info g_ccu_sensor_info[IMGSENSOR_SENSOR_IDX_MAX_NUM] = {{0} };
-static char g_ccu_sensor_name[IMGSENSOR_SENSOR_IDX_MAX_NUM][SENSOR_NAME_MAX_LEN];
+static struct ccu_sensor_info g_ccu_sensor_info
+	[IMGSENSOR_SENSOR_IDX_MAX_NUM];
+static char g_ccu_sensor_name
+	[IMGSENSOR_SENSOR_IDX_MAX_NUM][SENSOR_NAME_MAX_LEN];
 /*<<<<< Information get from imgsensor driver*/
 
 void ccu_set_current_fps(int32_t sensorType, int32_t current_fps)
@@ -51,30 +53,42 @@ void ccu_set_sensor_info(int32_t sensorType, struct ccu_sensor_info *info)
 		g_ccu_sensor_info[sensorType].i2c_id  = info->i2c_id;
 		if (info->sensor_name_string != NULL) {
 			memcpy(g_ccu_sensor_name[sensorType],
-			info->sensor_name_string, strlen(info->sensor_name_string)+1);
-			g_ccu_sensor_info[sensorType].sensor_name_string = g_ccu_sensor_name[sensorType];
+			info->sensor_name_string,
+			strlen(info->sensor_name_string)+1);
+			g_ccu_sensor_info[sensorType].sensor_name_string =
+			g_ccu_sensor_name[sensorType];
 		}
-		LOG_DBG_MUST("ccu catch sensor %d i2c slave address : 0x%x\n", sensorType, info->slave_addr);
-		LOG_DBG_MUST("ccu catch sensor %d name : %s\n",
-			sensorType, g_ccu_sensor_info[sensorType].sensor_name_string);
-		LOG_DBG_MUST("ccu catch sensor %d i2c_id : %d\n", sensorType, g_ccu_sensor_info[sensorType].i2c_id);
+		LOG_DBG_MUST("ccu catch sensor %d i2c slave address : 0x%x\n",
+		sensorType, info->slave_addr);
+		LOG_DBG_MUST("ccu catch sensor %d name : %s\n", sensorType,
+		g_ccu_sensor_info[sensorType].sensor_name_string);
+		LOG_DBG_MUST("ccu catch sensor %d i2c_id : %d\n", sensorType,
+		g_ccu_sensor_info[sensorType].i2c_id);
 	} else {
 		LOG_DBG_MUST("ccu catch sensor i2c slave address fail!\n");
 	}
 }
 
-void ccu_get_sensor_i2c_slave_addr(int32_t *sensorI2cSlaveAddr)
+void ccu_get_sensor_i2c_info(struct ccu_i2c_info *sensor_info)
 {
-	sensorI2cSlaveAddr[0] = g_ccu_sensor_info[IMGSENSOR_SENSOR_IDX_MAIN].slave_addr;
-	sensorI2cSlaveAddr[1] = g_ccu_sensor_info[IMGSENSOR_SENSOR_IDX_SUB].slave_addr;
-	sensorI2cSlaveAddr[2] = g_ccu_sensor_info[IMGSENSOR_SENSOR_IDX_MAIN2].slave_addr;
-	sensorI2cSlaveAddr[3] = g_ccu_sensor_info[IMGSENSOR_SENSOR_IDX_MAIN3].slave_addr;
+	int32_t i;
+
+	for (i = IMGSENSOR_SENSOR_IDX_MIN_NUM;
+		i < IMGSENSOR_SENSOR_IDX_MAX_NUM; ++i) {
+		sensor_info[i].slave_addr =
+		g_ccu_sensor_info[i].slave_addr;
+		sensor_info[i].i2c_id =
+		g_ccu_sensor_info[i].i2c_id;
+	}
 }
 
 void ccu_get_sensor_name(char **sensor_name)
 {
-	sensor_name[0] = g_ccu_sensor_info[IMGSENSOR_SENSOR_IDX_MAIN].sensor_name_string;
-	sensor_name[1] = g_ccu_sensor_info[IMGSENSOR_SENSOR_IDX_SUB].sensor_name_string;
-	sensor_name[2] = g_ccu_sensor_info[IMGSENSOR_SENSOR_IDX_MAIN2].sensor_name_string;
-	sensor_name[3] = g_ccu_sensor_info[IMGSENSOR_SENSOR_IDX_MAIN3].sensor_name_string;
+	int32_t i;
+
+	for (i = IMGSENSOR_SENSOR_IDX_MIN_NUM;
+		i < IMGSENSOR_SENSOR_IDX_MAX_NUM; ++i) {
+		sensor_name[i] =
+		g_ccu_sensor_info[i].sensor_name_string;
+	}
 }
