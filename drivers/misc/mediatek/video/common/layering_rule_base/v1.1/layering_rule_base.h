84e6981f6d6e850859e072a6a4c5091345b8442c
tai-hua.tseng <tai-hua.tseng@mediatek.com>
2020-01-02 14:17:58 +0800

    [ALPS04937580] DISP: fix APP resolution tuner HRT defect
    [Detail]
    In resize 2 layer scenario, HRT would arrange these layer into OVL0_2L.
    However, once GLES layer include resize layer. It would occur GLES layer
    assign to OVL0_2L, which is full-scale buffer config to before-scale
    ovl size. Then happened OVL underflow abnormal.
    [Solution]
    Adjust HRT & RPO scenario according to the result of GLES layer
    
    MTK-Commit-Id: 044874976995268c393312dd981e32dc30c2ab9b
    
    Change-Id: Ib1fd18546bd29375e7e1eb35c17ac866d2903223
    CR-Id:ALPS04937580
    Feature: [Module]Display Driver
    Signed-off-by: tai-hua.tseng <tai-hua.tseng@mediatek.com>
    (cherry picked from commit d279e9294c47dee19909e455babd71ac6ca8d517)

--- drivers/misc/mediatek/video/common/layering_rule_base/v1.1/layering_rule_base.h
+++ drivers/misc/mediatek/video/common/layering_rule_base/v1.1/layering_rule_base.h
@@ -151,6 +151,7 @@ struct layering_rule_ops {
 								int disp_idx);
 	bool (*adaptive_dc_enabled)(void);
 	bool (*adjust_hrt_level)(struct disp_layer_info *disp_info);
+	void (*adjust_hrt_scen)(struct disp_layer_info *disp_info);
 };
 
 #define HRT_GET_DVFS_LEVEL(hrt_num) (hrt_num & 0xF)
